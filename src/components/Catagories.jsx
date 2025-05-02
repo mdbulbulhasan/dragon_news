0fb2fdfb6249c7d10bfa5d1058261049aec7cc2d
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2025-05-02 23:22:28 +0600

    dynamically display catagories(20%)

--- src/components/Catagories.jsx
+++ src/components/Catagories.jsx
@@ -1,16 +1,28 @@
-import React, { use } from 'react';
+import React, { use } from "react";
+import { NavLink } from "react-router";
 
 // resolve a promise for fetching data
-const catagoriesPromise = fetch("/categories.json").then((res)=> res.json());
+const catagoriesPromise = fetch("/categories.json").then((res) => res.json());
 
 const Catagories = () => {
-    // for once rendaring data when load page use "use" hook
-    const catagories = use(catagoriesPromise);
-    return (
-        <div>
-            <h2 className='font-bold'>All Catagories ({catagories.length}) </h2>
-        </div>
-    );
+  // for once rendaring data when load page use "use" hook
+  const catagories = use(catagoriesPromise);
+  return (
+    <div>
+      <h2 className="font-bold">All Catagories ({catagories.length}) </h2>
+      <div className="grid grid-cols-1 mt-5 gap-3">
+        {catagories.map((catagory) => (
+          <NavLink
+            key={catagory.id}
+            className={"btn bg-base-100 border-0 hover:bg-base-200 font-semibold text-accent"}
+            to={`/catagory/${catagory.id}`}
+          >
+            {catagory.name}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Catagories;
--- src/pages/CatagoryNews.jsx
+++ src/pages/CatagoryNews.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const CatagoryNews = () => {
+    // how to use useParams 
+    const {id} = useParams();
+    console.log(id);
     return (
         <div>
-            Catagory News
+            Catagory News:{id}
         </div>
     );
 };
