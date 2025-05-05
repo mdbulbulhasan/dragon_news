5f4cd4c12d67949393849dbcfdecc59c0fadeeb8
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2025-05-05 10:57:52 +0600

    add auth context

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhF26YFfNBF934XqZojMfCW5XcHLkNSeM",
+  authDomain: "dragon-news-4c674.firebaseapp.com",
+  projectId: "dragon-news-4c674",
+  storageBucket: "dragon-news-4c674.firebasestorage.app",
+  messagingSenderId: "897940240514",
+  appId: "1:897940240514:web:6a7784c4182745aec39c9b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
